66a4d150c657d30cdfee4ea0b1d09bedd7105383
Jose Luis CG <142639379+JL1704@users.noreply.github.com>
2024-03-22 20:28:54 -0600

    Ejercicio "Ciclo de vida de un componente"

--- miAppMovil-2132939/src/app/home/home.page.ts
+++ miAppMovil-2132939/src/app/home/home.page.ts
@@ -7,16 +7,46 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor() {
+    console.log("MiComponenteComponent: Constructor");
+  }
+
+
 
   title: string = "Mi nueva pagina!";
 
-  hazmeClic(): void{
-    alert("Hiciste clic!");
-  }
+  //hazmeClic(): void{
+    //alert("Hiciste clic!");
+  //}
 
-  imgUrl: string = 'https://media.hswstatic.com/eyJidWNrZXQiOiJjb250ZW50Lmhzd3N0YXRpYy5jb20iLCJrZXkiOiJnaWZcL3BsYXlcL2E1NTFjMTA1LTdkNGMtNGE5NC1iM2UzLWZkM2NiYTg3MmIzYy0xOTIwLTEwODAuanBnIiwiZWRpdHMiOnsicmVzaXplIjp7IndpZHRoIjoiMTIwMCJ9fX0='
-  ngOnInit(){}
+  //imgUrl: string = 'https://media.hswstatic.com/eyJidWNrZXQiOiJjb250ZW50Lmhzd3N0YXRpYy5jb20iLCJrZXkiOiJnaWZcL3BsYXlcL2E1NTFjMTA1LTdkNGMtNGE5NC1iM2UzLWZkM2NiYTg3MmIzYy0xOTIwLTEwODAuanBnIiwiZWRpdHMiOnsicmVzaXplIjp7IndpZHRoIjoiMTIwMCJ9fX0='
+
+  ngOnChanges(){
+    console.log("MiComponenteComponent: OnChanges");
+  }
+  ngOnInit(){
+    console.log("MiComponenteComponent: OnInit");
+  }
+  ngDoCheck(){
+    console.log("MiComponenteComponent: DoCheck");
+  }
+  ngAfterContentInit(){
+    console.log("MiComponenteComponent: AfterContentInit");
+  }
+  ngAfterContentChecked(){
+    console.log("MiComponenteComponent: AfterContentChecked");
+  }
+  ngAfterViewInit(){
+    console.log("MiComponenteComponent: AfterViewInit");
+  }
+  ngAfterViewChecked(){
+    console.log("MiComponenteComponent: AfterViewChecked");
+  }
+  ngOnDestroy(){
+    console.log("MiComponenteComponent: OnDestroy");
+  }
+  
+
 
   verdadero: boolean = true;
 
