3b00f13913e53bb7f3511b6bd425d18a7c4c1958
Jose Luis CG <142639379+JL1704@users.noreply.github.com>
2024-04-19 21:19:12 -0600

    Comunicación entre componentes

--- miAppMovil-2132939/src/app/home/home.page.ts
+++ miAppMovil-2132939/src/app/home/home.page.ts
@@ -1,56 +1,82 @@
-import { Component } from '@angular/core';
-import { NavController } from '@ionic/angular';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { IonModal } from '@ionic/angular';
+import { OverlayEventDetail } from '@ionic/core';
+
+interface Tarea {
+  titulo: string;
+  descripcion: string;
+  fecha: string;
+  mes: number;
+  ano: number;
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
 
-  constructor(private navCtrl: NavController) {}
+export class HomePage implements OnInit {
+  @ViewChild(IonModal) modal: IonModal | undefined;
 
-  irAFormulario() {
-    this.navCtrl.navigateForward('/formulario');
-  }
-  
-
-
+  message = 'This modal example uses triggers to automatically open a modal when the button is clicked.';
+  name: string = '';
   title: string = "Mi App Tareas";
+  descripcionTarea: string = ''; 
 
-  //hazmeClic(): void{
-    //alert("Hiciste clic!");
-  //}
+  mesSeleccionado: string | undefined;
+  anoSeleccionado: string | undefined;
 
-  //imgUrl: string = 'https://media.hswstatic.com/eyJidWNrZXQiOiJjb250ZW50Lmhzd3N0YXRpYy5jb20iLCJrZXkiOiJnaWZcL3BsYXlcL2E1NTFjMTA1LTdkNGMtNGE5NC1iM2UzLWZkM2NiYTg3MmIzYy0xOTIwLTEwODAuanBnIiwiZWRpdHMiOnsicmVzaXplIjp7IndpZHRoIjoiMTIwMCJ9fX0='
+  tareas: Tarea[] = [];
 
-  ngOnChanges(){
-    console.log("MiComponenteComponent: OnChanges");
-  }
-  ngOnInit(){
-    console.log("MiComponenteComponent: OnInit");
-  }
-  ngDoCheck(){
-    console.log("MiComponenteComponent: DoCheck");
-  }
-  ngAfterContentInit(){
-    console.log("MiComponenteComponent: AfterContentInit");
-  }
-  ngAfterContentChecked(){
-    console.log("MiComponenteComponent: AfterContentChecked");
-  }
-  ngAfterViewInit(){
-    console.log("MiComponenteComponent: AfterViewInit");
-  }
-  ngAfterViewChecked(){
-    console.log("MiComponenteComponent: AfterViewChecked");
-  }
-  ngOnDestroy(){
-    console.log("MiComponenteComponent: OnDestroy");
-  }
-  
+  constructor() {}
 
+  ngOnInit() {
+    
+  }
 
-  verdadero: boolean = true;
+  openModal() {
+    if (this.modal) {
+      this.modal.present();
+    }
+  }
 
+  cancel() {
+    if (this.modal) {
+      this.modal.dismiss(null, 'cancel');
+    }
+  }
+
+  confirm() {
+    if (this.modal) {
+      if (!this.name || !this.descripcionTarea || !this.mesSeleccionado || !this.anoSeleccionado) {
+        console.error('Todos los campos deben estar completos.');
+        return;
+      }
+
+      const nuevaTarea: Tarea = {
+        titulo: this.name,
+        descripcion: this.descripcionTarea, 
+        fecha: `${this.mesSeleccionado}/${this.anoSeleccionado}`,
+        mes: parseInt(this.mesSeleccionado, 10), 
+        ano: parseInt(this.anoSeleccionado, 10) 
+      };
+
+      this.tareas.push(nuevaTarea);
+
+      this.name = '';
+      this.descripcionTarea = '';
+      this.mesSeleccionado = undefined;
+      this.anoSeleccionado = undefined;
+
+      this.modal.dismiss(null, 'confirm');
+    }
+  }
+
+  onWillDismiss(event: Event) {
+    const ev = event as CustomEvent<OverlayEventDetail<string>>;
+    if (ev.detail.role === 'confirm') {
+      this.message = `Hello, ${ev.detail.data}!`;
+    }
+  }
 }
